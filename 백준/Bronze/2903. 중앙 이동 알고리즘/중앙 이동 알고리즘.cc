f13c8987852731edc0155a1bd2b693c04f040733
GwangSoo <114225974+gs0428@users.noreply.github.com>
2023-06-18 00:39:01 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+    cout << (long long)pow(pow(2, N) + 1, 2);
+
+    return 0;
+}
