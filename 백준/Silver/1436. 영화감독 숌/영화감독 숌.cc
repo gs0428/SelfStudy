99ee09906e6a5063c86c6189add2ef9112b5bcb2
GwangSoo <114225974+gs0428@users.noreply.github.com>
2023-06-19 16:27:55 +0900

    [Silver V] Title: 영화감독 숌, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+    int start = 666;
+    int cnt = 0;
+
+    while(true) {
+        string temp = to_string(start);
+        if(temp.find("666") != string::npos)
+            cnt++;
+        if(N == cnt)
+            break;
+        start++;
+    }
+
+    cout << start;
+
+    return 0;
+}
+
