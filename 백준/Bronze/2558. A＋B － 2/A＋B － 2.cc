076710b4ef1aec6c3ac60c0e52d99224a588dd2b
GwangSoo <114225974+gs0428@users.noreply.github.com>
2023-06-21 23:44:19 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int A, B;
+    cin >> A >> B;
+    cout << A + B;
+
+    return 0;
+}
