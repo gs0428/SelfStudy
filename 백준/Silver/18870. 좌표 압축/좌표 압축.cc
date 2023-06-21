f21d8587af56ec399941f53b08b700ad59eab9ff
GwangSoo <114225974+gs0428@users.noreply.github.com>
2023-06-21 21:25:47 +0900

    [Silver II] Title: 좌표 압축, Time: 496 ms, Memory: 22256 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+    vector<long long> v(N, 0);
+    vector<long long> temp;
+    for(int i = 0;i<N;i++) {
+        cin >> v[i];
+        temp.push_back(v[i]);
+    }
+    sort(temp.begin(), temp.end());
+    temp.erase(unique(temp.begin(), temp.end()), temp.end());
+
+    for(int i = 0;i<N;i++) {
+        cout << lower_bound(temp.begin(), temp.end(), v[i]) - temp.begin() << " ";
+    }
+
+    return 0;
+}
