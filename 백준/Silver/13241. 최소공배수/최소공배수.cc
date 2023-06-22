e2de10a93df622930fc228db3a00cdea40252e2d
GwangSoo <114225974+gs0428@users.noreply.github.com>
2023-06-22 23:53:36 +0900

    [Silver V] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    long long int a, b;
+    cin >> a >> b;
+
+    long long int num = 2;
+    long long int mul = a * b;
+    long long int small = a < b ? a : b;
+
+    while(true) {
+        if(a % num == 0 && b % num == 0) {
+            mul /= num;
+            a /= num;
+            b /= num;
+        }
+        else
+            num += 1;
+
+        if(num > small)
+            break;
+    }
+
+    cout << mul;
+
+    return 0;
+}
