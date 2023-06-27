579bb37540953751f271e75a8bc2f4f12ad93197
GwangSoo <114225974+gs0428@users.noreply.github.com>
2023-06-27 19:55:38 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s;
+    cin >> s;
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << s << "::fan:\n";
+    cout << ":fan::fan::fan:\n";
+
+
+
+    return 0;
+}
