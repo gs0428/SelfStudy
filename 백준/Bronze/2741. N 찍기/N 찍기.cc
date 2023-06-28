c640901af1b7a2a1446c77b1ff5ec10ce868cee6
GwangSoo <114225974+gs0428@users.noreply.github.com>
2023-06-28 16:05:19 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int a;
+    cin >> a;
+    for (int i = 1; i <= a; i++) {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
