e309ad94323076d7cd777e31e124bd7fb678654f
GwangSoo <114225974+gs0428@users.noreply.github.com>
2023-06-29 22:26:45 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    vector<int> v(3, 0);
+
+    for (int i = 0; i < 3; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < 3; i++) {
+        cout << v[i] << " ";
+    }
+
+    return 0;
+}
