0cd676eef8daeec87b3e386277fa9565e40742d5
GwangSoo <114225974+gs0428@users.noreply.github.com>
2023-06-29 22:31:11 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int R1, S;
+    cin >> R1 >> S;
+    cout << 2 * S - R1;
+
+    return 0;
+}
