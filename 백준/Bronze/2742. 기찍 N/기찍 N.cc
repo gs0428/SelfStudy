5f3c84323f463d7710e138f51757663c38f58737
GwangSoo <114225974+gs0428@users.noreply.github.com>
2023-06-29 22:25:29 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    for (int i = n; i >= 1; i--) {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
