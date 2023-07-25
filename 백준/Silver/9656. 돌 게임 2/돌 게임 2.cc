d1c6e4d139c78a87a07ed6210aa8ab93b8f007f1
GwangSoo <114225974+gs0428@users.noreply.github.com>
2023-07-25 15:59:30 +0900

    [Silver IV] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <set>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    if(n % 2 == 0)
+        cout << "SK";
+    else
+        cout << "CY";
+
+    return 0;
+}
