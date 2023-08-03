2ed4191744dbebf4b045b5522e225249c567996a
GwangSoo <114225974+gs0428@users.noreply.github.com>
2023-08-03 13:40:37 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 188 ms, Memory: 9496 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n");
+input.map((s)=>console.log(s))
