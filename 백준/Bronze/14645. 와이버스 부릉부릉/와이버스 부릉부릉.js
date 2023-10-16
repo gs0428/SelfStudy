e132e5a19d57fabf1c68a339e82b6b43cf1ca3cf
GwangSoo <114225974+gs0428@users.noreply.github.com>
2023-10-16 17:43:54 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 112 ms, Memory: 9308 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+console.log('비와이')
