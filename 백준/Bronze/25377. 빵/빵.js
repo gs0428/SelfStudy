14b31ff19e4b634b51bd25ea48c9eada5479db6b
GwangSoo <114225974+gs0428@users.noreply.github.com>
2023-11-30 15:00:53 +0900

    [Bronze IV] Title: 빵, Time: 128 ms, Memory: 9360 KB, Score: 16 point -BaekjoonHub

---
+++ 백준/Bronze/25377. 빵/빵.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+let minute = 1001;
+input.slice(1).map((time) => {
+    const [A, B] = time.split(' ').map(Number);
+    if(B > A && B < minute) {
+        minute = B;
+    }
+})
+
+if(minute === 1001)
+    console.log(-1);
+else
+    console.log(minute);
