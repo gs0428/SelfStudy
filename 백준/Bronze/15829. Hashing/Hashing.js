70448cfad123468b88b91186946b0dc93f6ff80c
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-09-30 12:17:45 +0900

    [Bronze II] Title: Hashing, Time: 96 ms, Memory: 9336 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -3,9 +3,12 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 const str = input[1].split("");
 
-let result = 0;
+let result = 0n;
+const M = BigInt(1234567891);
 str.forEach((s, i) => {
-  result += Math.floor(((s.codePointAt() - 96) * Math.pow(31, i)) % 1234567891);
+  const a = BigInt(s.codePointAt() - 96);
+  const r = BigInt(31n ** BigInt(i));
+  result += a * r;
 });
 
-console.log(result);
+console.log((result % M).toString());
